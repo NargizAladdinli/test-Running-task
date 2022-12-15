0417dade264057ea877ea1c948df9f5386303df4
nargizaladdinli <nargizalieva05@gmail.com>
2022-12-15 22:03:50 +0400

    New task JAvascript

---
+++ wwork/src/js/app.js
@@ -0,0 +1,42 @@
+//undifned
+let numberOfPets = 5
+let age
+console.log(age);
+     
+
+// value
+
+age = "nice day"
+
+console.log(age);
+
+// Arrays
+
+const name = ["Stiven", "Alex", "Fines", "Ferb", "Pery", "Gery"];
+console.log(name);
+
+//object
+
+const city ={
+    olke: "Georia",
+    seher: "Tibili",
+}
+console.log(city);
+const country ={
+    olke: "Rusiya",
+    seher: "Moskva",
+}
+console.log(city);
+const fly ={
+    olke: "Doha",
+    seher: "Qeter",
+}
+console.log(city);
+
+//typeof
+
+console.log(typeof numberOfPets);
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof olke);
+console.log(typeof seher);
